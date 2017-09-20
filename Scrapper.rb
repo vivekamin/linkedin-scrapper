9e4afa992512d045836cba4fce8a648c69d33a4c
vivekamin <vivek@yodaplus.com>
2017-09-20 12:11:57 -0700

    scrapper script added

---
+++ Scrapper.rb
@@ -0,0 +1,38 @@
+require 'httparty'
+require 'nokogiri'
+require 'json'
+require 'pry'
+require 'csv'
+
+
+search = 0
+while search <=50 do
+slink = "https://www.google.com/search?q=linkedin+ARM+VLSI+profile&start=#{search}&sa=N"
+
+page = HTTParty.get(slink)
+search+=10
+
+parse_page = Nokogiri::HTML(page)
+#Pry.start(binding)
+
+#=begin
+
+links = Array.new()
+
+parse_page.css('.g').css('.r').map do |a|
+	
+	links.push(a.children.attribute('href').to_str.partition("=")[2].partition("&")[0])
+
+end
+
+
+
+links.each do |item|
+	if(item.include?("/in/"))
+		puts item
+	end
+end
+
+
+#=end
+end
